e59bc00dc8355a12a065e84e8c0b1516e7b4d191
sreedev ps <sreedev.p.skn@gmail.com>
2024-08-09 21:48:56 -0700

    Array generate from premutation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'twoSum';
+  value:any = ''
+  result:any[] = []
+
+  getValue(){
+    let input = JSON.parse(this.value)
+    for(let i=0; i<input.length; i++ ){
+      this.result[i] = input[input[i]]
+    }
+  }
 }
