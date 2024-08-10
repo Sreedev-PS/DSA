8e6c2b0bf6dbe64bdd8af13865ae85a25e730f14
Sreedev_PS <sreedev.p.skn@gmail.com>
2024-08-10 20:22:16 +0530

    Number of Good Pairs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'twoSum';
+  value:any = ''
+
+  numIdenticalPairs(nums:any): number {
+    nums = JSON.parse(nums)
+    let ans = 0;
+    const hashObject:any={};
+    for(let i=0;i<nums.length;i++){
+        if(hashObject[nums[i]] == undefined){
+            hashObject[nums[i]]=0;
+        }else{
+            hashObject[nums[i]]++;
+            ans = ans + hashObject[nums[i]];
+        }
+    }
+    return ans
+};
 }
